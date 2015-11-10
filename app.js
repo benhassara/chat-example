51c3b171e86a65af4b05d18ce5f9720f171c5305
benhassara <bhassara@gmail.com>
2015-11-10 14:39:10 -0700

    giphy integration

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+var request = require('request');
+
+var giphy = 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=';
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
@@ -23,4 +26,42 @@ io.on('connection', function(socket) {
         var output = msg.user + ': ' + msg.text;
         io.emit('chat message', output);
     });
+
+    socket.on('giphy', function(msg) {
+        var tags = msg.tags.trim().split(' ');
+
+        requestGiphy(tags)
+        .then(function(data) {
+            io.emit('giphy', {
+                text: msg.text,
+                gif: data.data.image_original_url
+            });
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+    });
 });
+
+
+function requestGiphy(tags) {
+    return new Promise(function(resolve, reject) {
+        var url = tags.reduce(function(prev, curr) {
+            prev += '+' + curr;
+            return prev;
+        }, giphy);
+
+        request(url, function(err, res, body) {
+            if (err) {
+                console.log(err);
+                reject('Error connecting to Giphy API');
+            }
+            if (res && res.statusCode === 200) {
+                resolve(JSON.parse(body));
+            }
+            else {
+                reject('Something weird happened.');
+            }
+        });
+    });
+}
--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -12,6 +12,13 @@ $(function() {
             input.attr('placeholder', 'Enter message.');
             $('#users').append($('<li>').html(input.val()));
         }
+        else if (input.val().trim().substr(0, 6) === '/giphy') {
+            var tags = input.val().substr(6).trim();
+            socket.emit('giphy', {
+                text: input.val(),
+                tags: tags
+            });
+        }
         else {
             socket.emit('chat message', {
                 user: user,
@@ -24,11 +31,25 @@ $(function() {
 
     socket.on('chat message', function(msg) {
         $('#messages').append($('<li>').html(msg));
+        scrollDown();
     });
 
     socket.on('new user', function(msg) {
         $('#messages').append($('<li>')
                       .css('color', 'red')
                       .html(msg));
+        scrollDown();
+    });
+
+    socket.on('giphy', function(msg) {
+        $('#messages').append($('<li>').html(user + ': ' + msg.text))
+        .append($('<li>').html(user + ': ')
+        .append($('<ul class="unstyled">')
+        .html('<li><img class="giphy-gif" src="' + msg.gif + '" /></li>')));
+        scrollDown();
     });
 });
+
+function scrollDown() {
+    $('html, body').animate({scrollTop: $(document).height()}, 'slow');
+}
